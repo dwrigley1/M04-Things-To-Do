6458f04ed4311a8b0b0d1154b50f6cdee9a91967
dwrigley1 <130004968+dwrigley1@users.noreply.github.com>
2024-04-10 05:59:33 -0400

    Add files via upload

---
+++ menagerie.py
@@ -0,0 +1,8 @@
+# Dakota Wrigley
+# 11.2 in 'Things To Do'
+# This program imports the zoo module as menagerie
+# Then calls its function from the module
+
+import zoo as menagerie
+menagerie.hours()
+
---
+++ zoo.py
@@ -0,0 +1,7 @@
+# Dakota Wrigley
+# 11.1 in 'Things To Do'
+# This program is designed to be a module that is called on later
+# It creates a custom function that prints to screen the hours of operation for the zoo 
+
+def hours():
+    print('Open 9-5 daily')
